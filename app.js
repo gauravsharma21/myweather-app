228146acb09ac037f3684344137e4f3b1c8ee68d
gauravsharma21 <41608359+gauravsharma21@users.noreply.github.com>
2019-04-18 21:08:49 +0530

    Format font style

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const chalk = require('chalk')
 
 const address = process.argv[2]
 if(!address){
@@ -8,15 +9,15 @@ if(!address){
 else{
     geocode(address, (err, data) => {
         if(err){
-            return console.log(err)
+            return console.log(chalk.red.inverse(err))
         }
     
             forecast(data.latitude, data.longitude, (error, forecastdata) => {
                 if(error){
-                    return console.log(error)
+                    return console.log(chalk.red.inverse(err))
                 }
-                console.log(data.location)
-                console.log(forecastdata)
+                console.log(chalk.green.inverse(data.location))
+                console.log(chalk.yellow.inverse(forecastdata))
               })
     })
 }
