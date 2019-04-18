6ceeaf009f20d0bfa9babd0160c9ad4ae72ace60
gauravsharma21 <41608359+gauravsharma21@users.noreply.github.com>
2019-04-18 23:46:07 +0530

    Destructuring and property shorthand

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const forecast = require('./utils/forecast')
 const chalk = require('chalk')
 
 const address = process.argv[2]
-if(!address){
+if (!address) {
     console.log(chalk.red.inverse("Please provide an address"))
 }
-else{
-    geocode(address, (err, data) => {
-        if(err){
+else {
+    geocode(address, (err, { latitude, longitude, location }) => {
+        if (err) {
             return console.log(chalk.red.inverse(err))
         }
-    
-            forecast(data.latitude, data.longitude, (error, forecastdata) => {
-                if(error){
-                    return console.log(chalk.red.inverse(err))
-                }
-                console.log(chalk.green.inverse(data.location))
-                console.log(chalk.yellow.inverse(forecastdata))
-              })
+
+        forecast(latitude, longitude, (error, forecastdata) => {
+            if (error) {
+                return console.log(chalk.red.inverse(err))
+            }
+            console.log(chalk.green.inverse(location))
+            console.log(chalk.yellow.inverse(forecastdata))
+        })
     })
 }
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,18 +1,18 @@
 const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZ2F1cmF2c2hhcm1hMjEiLCJhIjoiY2p1azY3aXZzMDFsYTRjbXFtNXIzb3NwMyJ9.bhDR7spBCvHtNlNsxOFLXw'
-    request({ url: url, json: true }, (err, Response) => {
-        if(err){
+    request({ url, json: true }, (err, { body }) => {
+        if (err) {
             callback('Cannot connect to network services!')
         }
-        else if(Response.body.features.length == 0){
+        else if (body.features.length == 0) {
             callback('Unable to find location. Try another search!')
         }
-        else{
+        else {
             callback(undefined, {
-                latitude : Response.body.features[0].center[1],
-                longitude : Response.body.features[0].center[0],
-                location : Response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
